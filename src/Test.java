fc6aa125cc9c49a9c73d835f387360d646932fd7
GGM-IT <G18790626507@qq.com>
2019-06-25 23:01:46 +0800

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+/**
+ * @author geng
+ * @data 2019/5/17/0:07
+ */
+public class Test {
+    public static void main(String[] args) {
+        int i = 1;
+        i=++i;
+        System.out.println(i);
+    }
+}
